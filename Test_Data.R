4a5ae13f40601e3a34c5dabc270817f4878ff998
Longping Fu <lpfu46@gatech.edu>
2025-07-01 08:14:37 -0400

    make cell surface 56 plot

--- Test_Data.R
+++ Test_Data.R
@@ -77,10 +77,84 @@ barplot_cell_surface_06252025_12_combined <- cell_surface_06252025_12_combined |
     ),
     position = position_dodge(0.9),
     vjust = -0.3,
-    size = 8
+    size = 5
   )
 
+# Cell Surface 06252025 5
+cell_surface_06252025_5 <- read_tsv(
+  '/Volumes/Expansion/Surface_Azide_06252025/ClosedSearch_56/Eclipse_LF_Surface_Azide_5_06252025/psm.tsv',
+  col_names = TRUE,
+  name_repair = 'universal'
+) |> 
+  select(
+    Modified.Peptide, Assigned.Modifications, Protein.ID
+  ) |> 
+  filter(
+    str_detect(Assigned.Modifications, '96.0440|71.0370')
+  ) |> 
+  distinct()
 
+cell_surface_06252025_5_H96 <- cell_surface_06252025_5 |> 
+  filter(
+    str_detect(Assigned.Modifications, '96.0440')
+  )
 
+cell_surface_06252025_5_K96 <- cell_surface_06252025_5 |> 
+  filter(
+    str_detect(Assigned.Modifications, '71.0370')
+  )
 
+# Cell Surface 06252025 6
+cell_surface_06252025_6 <- read_tsv(
+  '/Volumes/Expansion/Surface_Azide_06252025/ClosedSearch_56/Eclipse_LF_Surface_Azide_6_06252025/psm.tsv',
+  col_names = TRUE,
+  name_repair = 'universal'
+) |> 
+  select(
+    Modified.Peptide, Assigned.Modifications, Protein.ID
+  ) |> 
+  filter(
+    str_detect(Assigned.Modifications, '96.0440|71.0370')
+  ) |> 
+  distinct()
+
+cell_surface_06252025_6_H96 <- cell_surface_06252025_6 |> 
+  filter(
+    str_detect(Assigned.Modifications, '96.0440')
+  )
+
+cell_surface_06252025_6_K96 <- cell_surface_06252025_6 |> 
+  filter(
+    str_detect(Assigned.Modifications, '71.0370')
+  )
+
+# Result Combination 5&6
+cell_surface_06252025_56_combined <- tibble(
+  Exp = c('Exp5', 'Exp5', 'Exp6', 'Exp6'),
+  Modification = c('96.0440', '71.0370', '96.0440', '71.0370'),
+  Unique_Peptide = c(
+    cell_surface_06252025_5_H96 |> nrow(),
+    cell_surface_06252025_5_K96 |> nrow(),
+    cell_surface_06252025_6_H96 |> nrow(),
+    cell_surface_06252025_6_K96 |> nrow()
+  )
+)
+
+# Bar Plot
+barplot_cell_surface_06252025_56_combined <- cell_surface_06252025_56_combined |> 
+  ggplot() +
+  geom_bar(
+    aes(
+      x = Modification, y = Unique_Peptide, fill = Exp
+    ),
+    stat = 'identity', position = position_dodge()
+  ) +
+  geom_text(
+    aes(
+      x = Modification, y = Unique_Peptide, label = Unique_Peptide, group = Exp
+    ),
+    position = position_dodge(0.9),
+    vjust = -0.3,
+    size = 5
+  )
 
